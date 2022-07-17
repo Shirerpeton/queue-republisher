509ac1baf16bae0693733eafce1ba59748b316f8
Shirerpeton <xxxnekrosxxx@gmail.com>
2022-07-17 18:42:38 +0400

    add ability to filter messages based on content

--- src/queue-republisher.ts
+++ src/queue-republisher.ts
@@ -6,22 +6,24 @@ const program = new Command();
 program
     .name('queue-republisher')
     .description('simmple command line script to republish messages from one queue to another')
-    .version('0.1.0');
+    .version('1.0.0');
 
 program
     .option('-c, --connection-string <connection-string>', 'connection string to rabbitmq server', 'amqp://localhost:5672')
     .option('-s, --source-queue <source-queue>', 'source queue name', 'source-queue')
     .option('-t, --target-queue <target-queue>', 'target queue name', 'target-queue')
     .option('-m, --message-count <message-count>', 'number of messages to republish (set 0 or leave blank to not set a limit)', '0')
-    .option('-d, --delay <delay>', 'delay in milliseconds between each message', '0');
+    .option('-d, --delay <delay>', 'delay in milliseconds between each message', '0')
+    .option('-f, --filter <filter>', 'regex filter to apply message content (all messages that conform to filter will be repubslished to target queue and all otther will be republished back into source queue)', '')
 
 program.parse(process.argv);
 const options = program.opts();
 const connectionString: string = options.connectionString;
 const sourceQueueName: string = options.sourceQueue;
 const targetQueueName: string = options.targetQueue;
-const messageCount: number = parseInt(options.messageCount);
+let messageCount: number = parseInt(options.messageCount);
 const delay: number = parseInt(options.delay);
+const filter: string = options.filter;
 
 const closeChannel = async (channel: Channel): Promise<void> => {
     if (channel) await channel.close();
@@ -35,15 +37,21 @@ const closeConnection = async (connection: Connection): Promise<void> => {
     const getMessageHandler = (channel: Channel, queueName: string): ((content: string) => void) => {
         return content => channel.sendToQueue(queueName, Buffer.from(content));
     };
-    const consumeMessage = async (handler: (content: string) => void, delay: number): Promise<boolean> => {
+    const consumeMessage = async (targetHandler: (content: string) => void, sourceHandler: (content: string) => void, delay: number): Promise<boolean> => {
         const message: false | client.GetMessage = await sourceChannel.get(sourceQueueName);
         if(message == false) {
             console.log('No messages in queue');
             return false;
         }
         const messageContent: string = message.content.toString();
-        console.log(messageContent);
-        handler(messageContent);
+        console.log('Message received: ' + messageContent);
+        if(filter == '' || messageContent.match(filter)) {
+            console.log('publishing to target queue');
+            targetHandler(messageContent);
+        } else {
+            console.log('publishing to source queue');
+            sourceHandler(messageContent);
+        }
         sourceChannel.ack(message);
         return new Promise(resolve => setTimeout(() => resolve(true), delay));
     }
@@ -53,15 +61,16 @@ const closeConnection = async (connection: Connection): Promise<void> => {
     await targetChannel.assertQueue(targetQueueName);
     
     const sourceChannel: Channel = await connection.createChannel();
-    await sourceChannel.assertQueue(sourceQueueName);
+    const reply: client.Replies.AssertQueue = await sourceChannel.assertQueue(sourceQueueName);
     
-    let counter = 0;
-    while(messageCount == 0 || counter < messageCount) {
-        const result: boolean = await consumeMessage(getMessageHandler(targetChannel, targetQueueName), delay);
+    if(messageCount == 0 || messageCount > reply.messageCount) {
+        messageCount = reply.messageCount;
+    }
+    for(let i: number = 0; i < messageCount; i++) {
+        const result: boolean = await consumeMessage(getMessageHandler(targetChannel, targetQueueName), getMessageHandler(sourceChannel, sourceQueueName), delay);
         if(result == false) {
             break;
         }
-        counter++;
     }
     await closeChannel(targetChannel);
     await closeChannel(sourceChannel);
